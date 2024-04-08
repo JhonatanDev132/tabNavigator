277aedcfefdccde619c9d0f153346ddb70e78532
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-04-08 09:24:35 -0300

    .

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Ola from './src/screens/Ola'
+import Tchau from './src/screens/Tchau'
+
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Ola" component={Ola} options={{ headerShown: false }} />
+      <Tab.Screen name="Tchau" component={Tchau} />
+    </Tab.Navigator>
+  </NavigationContainer>
   );
 }
 
---
+++ src/screens/Tchau.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Tchau() {
+  return (
+    <View>
+      <Text>tchau</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
